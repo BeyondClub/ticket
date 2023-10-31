03e39d1f899363d2690fe8797ee55ec4252043db
ByteCrak07 <abilsavio@gmail.com>
2023-10-31 20:23:45 +0530

    🐛 Fix: updated tailwind config to fix packages/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	content: [
 		'./src/**/*.{tsx,ts,jsx,js,html}',
+		'./packages/ui/**/*.{tsx,ts,jsx,js,html}',
 		//  './../node_modules/@unlock-protocol/ui/dist/*.{js,css}'
 	],
 	presets: [require('./packages/ui/public/unlock-tailwind-preset')],

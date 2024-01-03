1f3ee0a3942459e8cdaf5d34f8f084bc4077a918
ByteCrak07 <abilsavio@gmail.com>
2024-01-03 12:18:07 +0530

    🐛 Fix: Event detail page translation

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,5 +4,9 @@ module.exports = {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
   },
+  localePath:
+    typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : '/locales',
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
